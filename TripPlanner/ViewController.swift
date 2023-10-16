d082a47e86e44b709d3516a58a150408db60a0d8
Tkhao Lanh <thaoltt1991@gmail.com>
2023-10-16 14:06:06 -0400

    Initial Commit

---
+++ TripPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripPlanner
+//
+//  Created by user248619 on 10/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
